600b865c53d3a5a6c1c4d9d29e32fce6f667bcb1
Roy Schrauwen <roy@royschrauwen.nl>
2022-03-21 20:27:25 +0100

    Material UI

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,18 +1,40 @@
 import React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import HomeIcon from "@mui/icons-material/Home";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import Paper from "@mui/material/Paper";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
 
 const Navbar = () => {
-  const handleNavClick = (event) => {
-    console.log(event.target.id);
-  };
+  const [value, setValue] = React.useState(0);
 
-  const navButtons = ["Home", "Show Reports", "New Report", "ToDo List"];
-  const buttons = navButtons.map((btn, key) => (
-    <button id={key} key={key} onClick={handleNavClick}>
-      {btn}
-    </button>
-  ));
-
-  return <div className="navbar">{buttons}</div>;
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label="New Report"
+          icon={<AddCircleOutlineIcon />}
+        />
+        <BottomNavigationAction label="Show Reports" icon={<ListAltIcon />} />
+        <BottomNavigationAction
+          label="ToDo List"
+          icon={<FormatListNumberedIcon />}
+        />
+      </BottomNavigation>
+    </Paper>
+  );
 };
 
 export default Navbar;
